43f525936a0f2a5b8446d04dccaab232ea0a1361
moonlight <mahtabsalarii04@gmail.com>
2025-05-31 01:42:55 +0330

    🐛 FIX: blogs

--- controllers/blog.js
+++ controllers/blog.js
@@ -16,20 +16,39 @@ export const getBlogById = (req, res) => {
 };
 export const createBlog = (req, res) => {
   const blogs = getblog();
-  const newBlog = { ...req.body };
+
+  const { authorId, authorName, title, thumbnail, published, commentsCount } =
+    req.body;
+  const newBlog = {
+    id: uuid(),
+    authorId,
+    authorName,
+    title,
+    thumbnail,
+    published,
+    commentsCount,
+  };
+
   blogs.push(newBlog);
   saveblog(blogs);
   res.status(201).json(newBlog);
 };
 export const updateBlog = (req, res) => {
   const blogs = getblog();
-  const index = blogs.findIndex((b) => b.id === req.params.id);
-  if (index === -1) {
-    return res.status(404).json({ error: "Blog not found" });
+
+  const blog = blogs.find((c) => c.id == req.params.id);
+  if (blog) {
+    blog.authorId = req.body.authorId || blog.authorId;
+    blog.authorName = req.body.authorName || blog.authorName;
+    blog.title = req.body.title || blog.title;
+    blog.thumbnail = req.body.thumbnail || blog.thumbnail;
+    blog.published = req.body.published || blog.published;
+    blog.commentsCount = req.body.commentsCount || blog.commentsCount;
+    res.json(blog);
+    saveblog(blogs);
+  } else {
+    res.status(404).json({ message: "blog not found" });
   }
-  blogs[index] = { ...blogs[index], ...req.body };
-  saveblog(blogs);
-  res.json(blogs[index]);
 };
 export const deleteBlog = (req, res) => {
   let blogs = getblog();
